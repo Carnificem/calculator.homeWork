b20286105c8ec7ad7d134d991ee21c0222c4facf
Carnificem <Carnificem1993@gmail.com>
2024-08-15 19:49:22 +0300

    Изменненный коммит

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -6,43 +6,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-    @RestController
-    @RequestMapping("/calculator")
+@RestController
+@RequestMapping("/calculator")
 
-    public class CalculatorController {
-        private final CalculatorService calculatorService;
+public class CalculatorController {
+    private final CalculatorService calculatorService;
 
-        public CalculatorController(CalculatorService calculatorService) {
-            this.calculatorService = calculatorService;
-        }
-
-
-        @GetMapping()
-        public String calculator() {
-            return ("Добро пожаловать в калькулятор");
-        }
-
-        @GetMapping(path = "/plus")
-        public String calculatorPlus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
-            return a + "+" + b + "=" + calculatorService.calculatorPlus(a, b);
-
-        }
-
-        @GetMapping(path = "/minus")
-        public String calculatorMinus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
-            return a + "-" + b + "=" + calculatorService.calculatorMinus(a, b);
-        }
-
-        @GetMapping(path = "/multiplay")
-        public String calculatorMultiplay(@RequestParam("num1") int a, @RequestParam("num2") int b) {
-            return a + "*" + b + "=" + calculatorService.calculatorMultiplay(a, b);
-        }
-
-        @GetMapping(path = "/divide")
-        public String calculatorDivide(@RequestParam("num1") int a, @RequestParam("num2") int b)throws Exception {
-
-            return a + "/" + b + "=" + calculatorService.calculatorDivide(a, b);
-
-        }
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
     }
+
+
+    @GetMapping()
+    public String calculator() {
+        return ("Добро пожаловать в калькулятор");
+    }
+
+    @GetMapping(path = "/plus")
+    public String calculatorPlus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + "+" + b + "=" + calculatorService.calculatorPlus(a, b);
+
+    }
+
+    @GetMapping(path = "/minus")
+    public String calculatorMinus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + "-" + b + "=" + calculatorService.calculatorMinus(a, b);
+    }
+
+    @GetMapping(path = "/multiplay")
+    public String calculatorMultiplay(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return a + "*" + b + "=" + calculatorService.calculatorMultiplay(a, b);
+    }
+
+    @GetMapping(path = "/divide")
+    public String calculatorDivide(@RequestParam("num1") int a, @RequestParam("num2") int b) throws Exception {
+
+        return a + "/" + b + "=" + calculatorService.calculatorDivide(a, b);
+
+    }
+}
 
--- src/main/java/com/example/demo/CalculatorService.java
+++ src/main/java/com/example/demo/CalculatorService.java
@@ -3,27 +3,29 @@ package com.example.demo;
 import org.springframework.stereotype.Service;
 
 
- @Service
-    public class CalculatorService {
+@Service
+public class CalculatorService {
 
 
-        public int calculatorPlus(int a, int b) {
-            return a + b;
-        }
-
-        public int calculatorMinus(int a, int b) {
-            return a - b;
-        }
-
-        public int calculatorMultiplay(int a, int b) {
-            return a * b;
-        }
-
-        public int calculatorDivide(int a, int b)throws Exception {
-            if (b==0) throw new Exception("На ноль делить нельзя");
-            else {return a / b;}
-
-
-        }
+    public int calculatorPlus(int a, int b) {
+        return a + b;
     }
+
+    public int calculatorMinus(int a, int b) {
+        return a - b;
+    }
+
+    public int calculatorMultiplay(int a, int b) {
+        return a * b;
+    }
+
+    public int calculatorDivide(int a, int b) throws Exception {
+        if (b == 0) throw new Exception("На ноль делить нельзя");
+        else {
+            return a / b;
+        }
+
+
+    }
+}
 
