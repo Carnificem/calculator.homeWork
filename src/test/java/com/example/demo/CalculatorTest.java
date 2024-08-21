40c9cd6c17aa851f90af0a69f846eecfc54588a5
Carnificem <Carnificem1993@gmail.com>
2024-08-21 15:45:23 +0300

    Изменненный коммит

--- src/test/java/com/example/demo/CalculatorTest.java
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -8,25 +8,31 @@ public class CalculatorTest {
 
     private final CalculatorService calculatorService = new CalculatorService();
 
+
     @Test
     public void calculatorPlus() {
         assertEquals(10, calculatorService.calculatorPlus(5, 5));
+        assertEquals(12, calculatorService.calculatorPlus(7, 5));
     }
 
     @Test
     void calculatorMinus() {
         assertEquals(0, calculatorService.calculatorMinus(5, 5));
+        assertEquals(-4, calculatorService.calculatorMinus(5, 9));
     }
 
     @Test
     void calculatorMultiplay() {
         assertEquals(25, calculatorService.calculatorMultiply(5, 5));
+        assertEquals(35, calculatorService.calculatorMultiply(5, 7));
     }
 
     @Test
     void calculatorDivide() throws IllegalArgumentException {
         assertEquals(1, calculatorService.calculatorDivide(5, 3));
+        assertEquals(10, calculatorService.calculatorDivide(50, 5));
     }
+
     @Test
     void calculatorDivisionByZero() throws IllegalArgumentException {
         assertEquals(1, calculatorService.calculatorDivide(5, 0));
